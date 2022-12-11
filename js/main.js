a886a085f133e7d6bdf76969a827e87395160bf4
Влад Устинов <lvpd.help@gmail.com>
2022-12-11 20:56:09 +0300

    half-work

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict'
+  
+    // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Зацикливайтесь на них и предотвращайте отправку
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
